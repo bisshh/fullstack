d563303a67491e3b2e7d652970d59bff19929cc6
unknown <reshamkhadka61@gmail.com>
2019-03-01 14:52:41 +0545

    using dynamic data

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,23 @@
 import React from 'react';
+
+const getYear = () => {
+    const newDate = new Date();
+    return newDate.getFullYear();
+}
+
+const user = {
+    name: 'Resham',
+    lastname: 'Khadka',
+    phone: 9849068407
+}
+
 const Header = () => {
-    return <header>This is the Header</header>
+    return (
+            <header>
+                <p>This is the Header. Today date is {getYear()}</p>
+                <p>My name is {user.name + ' ' + user.lastname} and my phone number is {user.phone}</p>
+            </header>
+        )
 }
 
 export default Header;
