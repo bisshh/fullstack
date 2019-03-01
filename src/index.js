cc44ca7d48ecacd027db1ead0d0b1b813a2325a2
unknown <reshamkhadka61@gmail.com>
2019-03-01 14:15:33 +0545

    add element inside the content

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () =>{
-    return React.createElement('h1',{className:'title'},'hello world!!') //first argument is a tag, second argument is class or function and last is a content
+    return React.createElement('h1',{className:'title'},React.createElement('div')) //first argument is a tag, second argument is class or function and last is a content
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
